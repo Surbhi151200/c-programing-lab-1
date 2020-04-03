5b3a4111e3090e5354802e33c4f49cc8502c9299
Surbhi151200 <60388202+Surbhi151200@users.noreply.github.com>
2020-04-03 12:50:15 +0530

    Create finding alphabet or operator.c

---
+++ finding alphabet or operator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	char n;
+	printf(" Enter a character (alphabet,number or operator)  :  ");
+	scanf("%c",&n);
+    if(((n>=97)&&(n<=122))||((n>=65)&&(n<=90)))
+   	printf(" Character Is An Alphabet");
+     	else
+       	printf(" Character Is Not An Alphabet");
+	return 0;
+}
