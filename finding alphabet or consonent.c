2160329eac4647c2b612a05302eea7a5c16ab59e
Surbhi151200 <60388202+Surbhi151200@users.noreply.github.com>
2020-04-03 12:51:37 +0530

    Create finding alphabet or consonent.c

---
+++ finding alphabet or consonent.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	char n;
+	printf(" Enter an alphabet  :  ");
+	scanf("%c",&n);
+    if((n=='a')||(n=='e')||(n=='i')||(n=='o')||(n=='u')||(n=='A')||(n=='E')||(n=='I')||(n=='O')||(n=='U'))
+   	printf(" Alphabet is a vowel ");
+     	else
+       	printf(" Alphabet is a consonent ");
+	return 0;
+}
