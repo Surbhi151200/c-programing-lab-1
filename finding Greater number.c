6a8ad49d9f85d2a2d49b63ae49d0c6247e761cac
Surbhi151200 <60388202+Surbhi151200@users.noreply.github.com>
2020-04-03 12:39:43 +0530

    Create finding Greater number.c

---
+++ finding Greater number.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+int main()
+{
+
+	int n,m;
+	
+  printf(" Enter a Number  :  ");
+	scanf("%d",&n);
+	
+  printf(" Enter a Number  :  ");
+	scanf("%d",&m);
+	
+  if(n>m)
+  
+  printf("Greater number is  :  %d",n);
+   	else
+  
+  printf("Greater number is  :  %d",m);
+	return 0;
+}
