ff4039148c2ae400e8c355b7c70365448032d0cd
Surbhi151200 <60388202+Surbhi151200@users.noreply.github.com>
2020-04-03 12:53:16 +0530

    Create finding Alphabet digit character.c

---
+++ finding Alphabet digit character.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<ctype.h>
+int main()
+{
+	char n;
+	printf(" Enter a character (alphabet,number or operator)  :  ");
+	scanf("%c",&n);
+	if(isalpha(n))
+   	printf(" Character is an Alphabet");
+     	else
+       	if(isdigit(n))
+         	printf(" Character is a digit");
+           	else
+             	printf(" Character is a special character");
+	return 0;
+}
